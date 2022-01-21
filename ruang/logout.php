81e7385b55f53a8c4244acf4f0f5d05464d7b29d
Thanhthuan020 <71782129+cabupig036@users.noreply.github.com>
2022-01-21 10:26:37 +0700

    Delete ruang directory

--- ruang/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-   include('./conndb.php');
-   session_start();
-    unset($_SESSION['user']);
-    echo '<script language="javascript">alert("Đăng xuất thành công"); window.location="../Anyar/index.html";</script>';
-    exit;
-    
-?>
